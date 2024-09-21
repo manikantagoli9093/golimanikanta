9cd772e43278e8e61bd10307c038556f9f7e01e6
manikantagoli7077 <manikantagoli7077@gmail.com>
2024-09-21 12:59:24 +0530

    warnings removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import ContactForm from './components/Contact';
+
 import Home from './components/Home';
-import Navbar from './components/Navbar';
+
 
 function App() {
   return (
